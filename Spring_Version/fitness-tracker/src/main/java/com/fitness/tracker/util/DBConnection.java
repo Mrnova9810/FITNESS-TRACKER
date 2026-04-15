718e80d6e55f685f10a84fac63ea69da46ce0a5a
Mr.Nova@9810 <surajduse@gmail.com>
2026-04-15 21:05:38 +0530

    DB POTGRESQL added

--- Spring_Version/fitness-tracker/src/main/java/com/fitness/tracker/util/DBConnection.java
+++ Spring_Version/fitness-tracker/src/main/java/com/fitness/tracker/util/DBConnection.java
@@ -5,13 +5,24 @@ import java.sql.DriverManager;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/fitness_db";
-    private static final String USER = "root";
-    private static final String PASSWORD = "1234";
+
+    // locally
+//    private static final String URL = "jdbc:mysql://localhost:3306/fitness_db";
+//    private static final String USER = "root";
+//    private static final String PASSWORD = "1234";
+
+
+    private static final String URL = System.getenv("SPRING_DATASOURCE_URL");
+    private static final String USER = System.getenv("SPRING_DATASOURCE_USERNAME");
+    private static final String PASSWORD = System.getenv("SPRING_DATASOURCE_PASSWORD");
 
     public static Connection getConnection() throws Exception {
 
-        Class.forName("com.mysql.cj.jdbc.Driver");
+//        Class.forName("com.mysql.cj.jdbc.Driver");
+        Class.forName("org.postgresql.Driver");
+
+
+
 
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
